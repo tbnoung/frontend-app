8a926d478a0cc736c498668a4a182065e469b593
Theerasak Buakai <theerasak.bu@inet.co.th>
2020-12-02 00:46:39 +0700

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Welcome Tbnoung web site</h1>
   </div>
 </template>
 
