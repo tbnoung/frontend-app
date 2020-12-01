5519fac8fa316ff6a2b5f47b1d0ecdbf27452c79
Theerasak Buakai <theerasak.bu@inet.co.th>
2020-12-02 00:52:36 +0700

    a

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,3 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
